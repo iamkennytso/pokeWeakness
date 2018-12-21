53a97c3fce7c666032a14a821d5b05b3326c6ee9
Kenneth Tso <iamkennytso@gmail.com>
2018-12-21 02:33:17 -0500

    select, img

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import SearchBar from './components/SearchBar/SearchBar'
+
+const INITIAL_STATE = {
+  selectedPokemon: {
+    label: 'Pikachu',
+    value: 'pikachu'
+  },
+  history: [],
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+  };
+
+  /** 
+  * Handle Selected Change
+  * use case: when a pokemon is selected via the Search Bar or History Tracker, sets the selectedPokemon state.
+  * @param {Object} selectedPokemon - object consisting of: {
+  *   label: camel case name of pokemon
+  *   value: lower case name of pokemon
+  * }
+  */
+  _handleSelectedChange = selectedPokemon => {
+    this.setState({selectedPokemon});
+  }
+
   render() {
+    const { selectedPokemon } = this.state;
+    const imgUrl = selectedPokemon.value
+      // for the Alolans 
+      .replace(/\s\(alo\)/, '-alolan')
+      // for Mr. Mime
+      .replace(/.\s/, '-')
+      // for Farfetch'd
+      .replace(/'/, '');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBar 
+          selectedPokemon={selectedPokemon}
+          handleChange={this._handleSelectedChange}
+        />
+        <img 
+          src={`https://img.pokemondb.net/artwork/large/${imgUrl}.jpg`} 
+          alt={`${selectedPokemon}`}
+        />
       </div>
     );
   }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import Pokemon from '../../constants/pokemon'
+import './SearchBar.scss';
+
+const options = Object.keys(Pokemon).map(poke => {
+  return {
+    value: poke.toLowerCase(),
+    label: poke,
+  }
+})
+
+const SearchBar = props => {
+  return (
+    <div className="SearchBar">
+    <Select
+      value={props.selectedPokemon}
+      onChange={props.handleChange}
+      options={options}
+    />
+  </div>
+  );
+};
+
+export default SearchBar;
