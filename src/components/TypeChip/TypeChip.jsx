654cd6617cd0bb6f5aa402e04e32d2494b7aabd2
Kenneth Tso <iamkennytso@gmail.com>
2019-01-04 14:37:38 -0500

    weakness icons

--- src/components/TypeChip/TypeChip.jsx
+++ src/components/TypeChip/TypeChip.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import './TypeChip.scss';
+import Bug from './Bug.png';
+import Dark from './Dark.png';
+import Dragon from './Dragon.png';
+import Electric from './Electric.png';
+import Fairy from './Fairy.png';
+import Fighting from './Fighting.png';
+import Fire from './Fire.png';
+import Flying from './Flying.png'
+import Ghost from './Ghost.png';
+import Grass from './Grass.png';
+import Ground from './Ground.png';
+import Ice from './Ice.png';
+import Normal from './Normal.png';
+import Poison from './Poison.png';
+import Psychic from './Psychic.png';
+import Rock from './Rock.png';
+import Steel from './Steel.png';
+import Water from './Water.png';
 
 const multiplierToString = {
   .25: 'Quarter',
@@ -9,10 +27,56 @@ const multiplierToString = {
   4: 'Quadruple',
 }
 
+const getPicture = type => {
+  switch(type){
+    case 'Bug':
+      return Bug;
+    case 'Dark':
+      return Dark;
+    case 'Dragon':
+      return Dragon;
+    case 'Electric':
+      return Electric;
+    case 'Fairy':
+      return Fairy;
+    case 'Fighting':
+      return Fighting;
+    case 'Fire':
+      return Fire;
+    case 'Flying':
+      return Flying;
+    case 'Ghost':
+      return Ghost;
+    case 'Grass':
+      return Grass;
+    case 'Ground':
+      return Ground;
+    case 'Ice':
+      return Ice;
+    case 'Normal':
+      return Normal;
+    case 'Poison':
+      return Poison;
+    case 'Psychic':
+      return Psychic;
+    case 'Rock':
+      return Rock;
+    case 'Steel':
+      return Steel;
+    case 'Water':
+      return Water;
+  }
+};
+
 const TypeChip = ({ type, effectiveness }) => {
   return (
-    <div className={`Weaknesses-Chip Weakness-Chip-${multiplierToString[effectiveness]}`}>
-      {type}: {effectiveness}
+    <div className={`Weakness-Chip-${multiplierToString[effectiveness]}`}>
+      <img
+        className={`Weakness-Chip-${multiplierToString[effectiveness]}-Img`}
+        src={getPicture(type)}
+        alt={`${type}`}
+      />
+      {effectiveness}
     </div>
   );
 };
